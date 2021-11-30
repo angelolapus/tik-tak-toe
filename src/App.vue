c8ffa0d886149f09a0b06626d0a985305776e66f
Angelo Paulo Lapus <angelo.paulo.a.lapus@accenture.com>
2021-11-30 17:48:47 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+    <WelcomeScreen title="Tik-tak Toe" gameScreenBtnCaption="Start"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  import  WelcomeScreen  from './components/WelcomeScreen.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      WelcomeScreen
+    }
+  } 
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  background-color: #FFCC33;
 }
 </style>
---
+++ src/components/WelcomeScreen.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="welcomeScreen">
+        <h1 class="game-title">{{title}}</h1>
+        <button class="game-button">{{gameScreenBtnCaption}}</button>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'WelcomeScreen',
+  props:{
+      title: String,
+      gameScreenBtnCaption: String
+  }
+} 
+</script>
+
+<style lang="scss">
+    @import '../styles/variable.scss';
+    .welcomeScreen{
+        border: 1px solid $color-green;
+
+        .game-title{
+            border:1px solid $color-blue;
+        }
+    }
+</style>
