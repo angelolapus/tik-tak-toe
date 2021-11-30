bd0671b49c8a279835c4bd668445696c8060c5fa
Angelo Paulo Lapus <angelo.paulo.a.lapus@accenture.com>
2021-11-30 18:28:55 +0800

    added partial game board

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,20 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/>-->
 
     <WelcomeScreen title="Tik-tak Toe" gameScreenBtnCaption="Start"/>
+    <Board></Board>
   </div>
 </template>
 
 <script>
 
   import  WelcomeScreen  from './components/WelcomeScreen.vue';
+  import Board from './components/Board.vue';
 
   export default {
     name: 'App',
     components: {
-      WelcomeScreen
+      WelcomeScreen,
+      Board
     }
   } 
 
---
+++ src/components/Board.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="board-container">
+        <div v-for="(item,index) in boardItem" 
+        v-bind:key="item.id"
+        v-bind:item ="item"
+        v-bind:index = "index"
+        class="boardItem">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Board',
+    data: ()=> {
+        return{
+            name: 'angelo lapus',
+            boardItem:[
+                {id: 0, value: ''},
+                {id: 1, value: ''},
+                {id: 2, value: ''},
+                {id: 3, value: ''},
+                {id: 4, value: ''},
+                {id: 5, value: ''},
+                {id: 6, value: ''},
+                {id: 7, value: ''}
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    @import '../styles/variable.scss';
+    .board-container{
+        .boardItem{
+            width: 68px;
+            height: 68px;
+            border: 2px solid $color-black;
+        }
+    }
+</style>
